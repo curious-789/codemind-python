919d3b5a69f30438c74bce29f9b11b6c00ea6825
charan <84625492+curious-789@users.noreply.github.com>
2022-11-03 10:34:25 +0530

    t=int(input()) if t%9==0:     print(9) else:     print(t%9)

--- Add_Digits.py
+++ Add_Digits.py
@@ -1,10 +1,5 @@
-def digSum(n):
-    if n==0:
-        return 0
-    if n%9==0:
-        return 9
-    else:
-        return n%9
-n=int(input())
-print(digSum(n))
-        
+t=int(input())
+if t%9==0:
+    print(9)
+else:
+    print(t%9)
